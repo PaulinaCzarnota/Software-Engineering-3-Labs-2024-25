49416ddf04f0222781fc902491b67685a9e4a733
Paulina Czarnota <92259754+PaulinaCzarnota@users.noreply.github.com>
2024-11-11 21:08:47 +0000

    Delete Software-Engineering-3-Labs-2024-25-main directory

--- Software-Engineering-3-Labs-2024-25-main/Week 5/SE3_JDBC_Example/src/CustomerDao.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface CustomerDao {
-    List<Customer> selectCustomersByName(String name);
-    Customer findCustomerById(int customerNumber);
-    void updateCustomer(double credLim, int custID);
-    void deleteCustomer(int customerNumber);
-}
